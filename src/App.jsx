89777aea6d1eabe87593983bf1473952f82212d8
thinh205171 <75664882+thinh205171@users.noreply.github.com>
2023-10-13 11:24:08 +0700

    init project, add libraries

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="h-header bg-main w-full  ">
+      <h1 className="text-3xl text-star font-bold underline">Hello World</h1>
+      <p></p>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#080A1A",
+        subMain: "#F20000",
+        dry: "#0B0F29",
+        star: "#FFB000",
+        text: "#C0C0C0",
+        border: "#4b5563",
+        dryGray: "#E0D5D5",
+      },
+
+      height: {
+        header: "560px",
+        rate: "400px",
+      },
+    },
+  },
+  plugins: [],
+};
