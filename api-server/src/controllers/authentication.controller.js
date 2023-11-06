1aa947946d8179896f1a5de9dc7fd182c3a5cf7c
stewie2k2 <n.hghuy1311@gmail.com>
2023-11-06 21:34:57 +0700

    add login function

--- api-server/src/controllers/authentication.controller.js
+++ api-server/src/controllers/authentication.controller.js
@@ -1,5 +1,6 @@
 const accountUtils = require('../utils/account.util');
-const accountConfig = require('../configs/account.config')
+const accountConfig = require('../configs/account.config');
+const accountService = require("../services/account.service");
 const {bcryptHash} = require("./encryption.util");
 
 exports.signup = async (req, res) => {
@@ -33,13 +34,25 @@ exports.signup = async (req, res) => {
 exports.signin = async (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
-    const errorMsg = [];
 
-    await accountUtils.checkIfEmailExists(email, errorMsg);
+    await accountService.checkPassword(email).then(user => {
+        if(!user){
+            res.status(400).json({ message: 'Invalid email or password' });
+        }else {
+            bcrypt.compare(password, user.password).then(result => {
+                    if (result) {
+                        console.log("Login successful!");
+                        res.status(200).json({user: {
+                            email: user.email,
+                            username: user.username,
+                        }});
+                    } else {
+                        console.log('Invalid email or password');
+                        res.status(400).json({ message: 'Invalid email or password' });
+                    }
+                }).catch(error => console.error('Error comparing passwords:', error));
+        }
+    });
 
-    if (errorMsg.indexOf(accountConfig.email.existErrMsg) < 0) {
-        res.status(400).json({status: "fail", message: "Login failed"})
-        return
-    }
 
 }
--- api-server/src/services/account.service.js
+++ api-server/src/services/account.service.js
@@ -1,6 +1,11 @@
-exports.saveUser = (user) => {
+let userModel = require("../models/user.model")
+
+exports.saveUser = async (user) => {
     throw new Error("Not implemented");
 };
-exports.getUserByUsernameAndEmail = (username, email) => {
+exports.getUserByUsernameAndEmail = async (username, email) => {
     throw new Error("Not implemented");
 };
+exports.getUserByEmail = async (email) => {
+    return userModel.findOne({email : email});
+}
