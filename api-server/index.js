b882778bc45507cdd93a07a3e6a862a9334c7baa
dev1de <davidhuy1311@gmail.com>
2023-12-03 01:44:04 +0700

    add guard, change route name

--- api-server/index.js
+++ api-server/index.js
@@ -4,6 +4,7 @@ const db = require('./src/configs/db.config');
 const session = require('express-session');
 
 const authRoute = require('./src/routes/authentication.route');
+const { verifyToken } = require('./src/utils/account.util');
 
 const app = express();
 
@@ -20,7 +21,7 @@ app.get('/ping', (req, res) => {
     res.json({'status': 'ok'});
 });
 
-app.use('/api', authRoute);
+app.use('/auth', authRoute);
 
 const port = SERVER.PORT || 3000;
 app.listen(port, '0.0.0.0', () => {
