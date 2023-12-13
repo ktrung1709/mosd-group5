0627dca1fa241d06a729856383838862a5b7a747
FAP6402 <hoangvanphuong6402@gmail.com>
2023-12-13 16:06:55 +0700

    forgot reset

--- api-server/src/routes/authentication.route.js
+++ api-server/src/routes/authentication.route.js
@@ -6,5 +6,7 @@ const router = express.Router();
 router.post("/signup", authController.signup);
 router.post("/signin", authController.signin);
 router.get("/logout", authController.logout);
+router.post("/forgot-password", authController.forgotPassword);
+router.post("/reset-password", authController.resetPassword);
 
 module.exports = router;
