f78e8238aca358e6c33396aaed15afe02699aa98
stewie2k2 <n.hghuy1311@gmail.com>
2023-12-29 00:36:42 +0700

    crud watch_list + favorite + recent_view

--- api-server/src/services/userInfo.service.js
+++ api-server/src/services/userInfo.service.js
@@ -1,35 +1,70 @@
 let User = require("../models/user.model")
 
 exports.getInfo = async (userId) => {
-    return await User.findById(userId);
+    return await User.findById(userId).select("-password");
 };
 
-exports.getMovies = async(userId) => {
-    await User.findById(userId).select("recent_view favorite watch_list").then(user => {
-        return user;
-    }).catch(error => console.log(error));
+exports.getMovies = async (userId) => {
+    return await User.findById(userId).select("recent_view favorite watch_list");
 }
 
 exports.getFavorite = async (userId) => {
-    await User.findById(userId).select("favorite").then(user => {
-        return user;
-    }).catch(error => console.log(error));
+    return await User.findById(userId).select("favorite");
 };
 
 exports.getRecent = async (userId) => {
-    await User.findById(userId).select("recent_view").then(user => {
-        return user;
-    }).catch(error => console.log(error));
+    return await User.findById(userId).select("recent_view");
 };
 
 exports.getListInfo = async (userId, watchListName) => {
-    await User.findById(userId).select('watch_list').where('watch_list.name').equals(watchListName).then(user => {
-        return user;
-    }).catch(error => console.log(error));
+    return await User.findById(userId).select('watch_list').where('watch_list.name').equals(watchListName);
 };
 
 exports.getLists = async (userId) => {
-    await User.findById(userId).select("watch_list.name").then(user => {
-        return user;
-    }).catch(error => console.log(error));
+    return await User.findById(userId).select("watch_list.name");
+}
+
+exports.addToFavorite = async (userId, movieId) => {
+    return await User.findByIdAndUpdate(userId, { $push: { favorite: movieId } }, { new: true });
+}
+
+exports.addToRecent = async (userId, movieId) => {
+    return await User.findByIdAndUpdate(userId, { $push: { recent_view: movieId } }, { new: true });
+}
+
+exports.addToList = async (userId, movieId, listName) => {
+    return await User.updateOne({
+        _id: userId,
+        'watch_list.list_name': listName,
+        },
+        {
+            $addToSet: {
+                'watch_list.$.movie_ids': movieId,
+            },
+        }, 
+        { new: true });
+}
+
+exports.createList = async (userId, listName) => {
+    return await User.findByIdAndUpdate(userId, { $push: { watch_list: { list_name: listName } } }, { new: true });
+}
+
+exports.deleteList = async (userId, listName) => {
+    return await User.findByIdAndUpdate(userId, { $pull: { watch_list: { list_name: listName } } }, { new: true });
+}
+
+exports.deleteFromList = async (userId, movieId, listName) => {
+    return await User.updateOne({
+        _id: userId,
+        'watch_list.list_name': listName,
+      },
+      {
+        $pull: {
+          'watch_list.$.movie_ids': movieId,
+        },
+      });
+}
+
+exports.deleteFromFavorite = async (userId, movieId) => {
+    return await User.findByIdAndUpdate(userId, { $pull: { favorite: movieId } }, { new: true });
 }
