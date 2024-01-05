0dbb0e1961e53b3ced20ca8b3bdbada048b25d62
thinh205171 <75664882+thinh205171@users.noreply.github.com>
2024-01-06 02:52:19 +0700

    update response userId after login

--- api-server/src/controllers/authentication.controller.js
+++ api-server/src/controllers/authentication.controller.js
@@ -48,13 +48,14 @@ exports.signin = async (req, res) => {
             res.status(400).json({ message: 'Invalid email or password' });
         } else {
             bcrypt.compare(password, user.password).then(result => {
-
+                console.log(user)
                 if (result) {
                     const token = jwt.sign({ id: user.id, username: user.username }, SECRET_KEY, { expiresIn: '2 days' });
                     res.status(200).json({
                         status: 'ok',
                         token: token,
-                        username: username
+                        username: username,
+                        userId: user._id
                     });
                 } else {
                     res.status(400).json({ message: 'Invalid email or password' });
