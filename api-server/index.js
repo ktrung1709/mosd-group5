e6b8c727397385a10082e68fb10191108482f063
vcth4nh <tvu25337@gmail.com>
2024-01-07 14:59:06 +0700

    add crud feedback

--- api-server/index.js
+++ api-server/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const { SERVER } = require('./src/configs/main.config');
+const {SERVER} = require('./src/configs/main.config');
 const db = require('./src/configs/db.config');
-const { verifyToken } = require('./src/utils/account.util');
+const {verifyToken} = require('./src/utils/account.util');
 
 const authRoute = require('./src/routes/authentication.route');
 const userInfoRoute = require('./src/routes/userInfo.route');
 const accountRoute = require('./src/routes/account.route');
 const movieRoute = require('./src/routes/movie.route');
-
+const feedbackRoute = require('./src/routes/feedback.route');
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
@@ -22,16 +22,17 @@ app.use((req, res, next) => {
 
 
 app.get('/ping', (req, res) => {
-    res.json({ 'status': 'ok' });
+    res.json({'status': 'ok'});
 });
 
 app.get('/ping-auth', verifyToken, (req, res) => {
-    res.json({ 'status': 'ok' });
+    res.json({'status': 'ok'});
 });
 
 app.use('/auth', authRoute);
 app.use('/account', accountRoute);
 app.use('/movie', movieRoute);
+app.use('/feedback', verifyToken, feedbackRoute);
 
 app.use('/user', verifyToken, userInfoRoute);
 
---
+++ api-server/src/utils/feedback.util.js
@@ -0,0 +1,35 @@
+const {Types} = require('mongoose');
+const feedbackService = require("../services/feedback.service");
+
+function sanitizeFeedback(data, movieId, userId) {
+    let feedback = {}
+    feedback.movie = movieId;
+    feedback.user = userId;
+    feedback.time = Date.now();
+    feedback.comment = data.comment ?? null;
+    feedback.rate = data.rate ?? null;
+    if (feedback.rate < 0 || feedback.rate > 10) {
+        throw new Error('Rate must be in range [0, 10]');
+    }
+    if (feedback.comment === null && feedback.rate === null) {
+        throw new Error('Feedback must have at least one of comment or rate');
+    }
+    return feedback;
+}
+
+function sanitizeMovieID(movieId) {
+    if (!Types.ObjectId.isValid(movieId)) {
+        throw new Error('Invalid movie ID');
+    }
+    return new Types.ObjectId(movieId);
+}
+
+async function getFeedbackIDByMovieAndUser(userId, movieId, force = true) {
+    let feedbackId = (await feedbackService.getFeedbackByMovieAndUser(userId, movieId))[0]?._id;
+    if (!feedbackId && force) {
+        throw new Error('Feedback not found');
+    }
+    return feedbackId;
+}
+
+module.exports = {sanitizeFeedback, sanitizeMovieID, getFeedbackIDByMovieAndUser};
